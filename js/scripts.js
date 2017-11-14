3c871f5db94e9b6b9543226176aa48bc1da2eca6
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2017-11-14 21:08:10 +1300

    Moved to new username

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+
+// $(function() {
+// 	$('body')
+// 		.addClass('docLoading')
+// 		.append('\
+// 		<div class="loading">\
+// 			<div>\
+// 				<img src="img/gears.gif">\
+// 				<p>Loading<br>Please wait</p>\
+// 			</div>\
+// 		</div>\
+// 	')
+// })
+
+// $(window).on('load',function() {
+// 	$(document).find('.loading').remove()
+// 	$('body').removeClass('docLoading')
+// })
