c080bb348adc28b4c12b0922754796dd35e18a9e
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2017-11-16 16:31:57 +1300

    scrolling function

--- js/scripts.js
+++ js/scripts.js
@@ -26,3 +26,10 @@ $(function() {
 		$('#navItems').slideToggle('fast').css('display', 'inline-block')
 	})
 })
+
+var scrollSpeed = 400
+
+function winScroll(ele) {
+	var top = $(ele.attr('rel')).offset().top
+	$('html, body').animate({scrollTop: top}, scrollSpeed);
+}
